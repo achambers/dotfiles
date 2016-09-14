6183f1a6fae37fac0fbad4ea93e8a98a4c6127ae
Aaron Chambers <achambers@gmail.com>
2016-09-14 13:46:40 +0100

    Added new files

---
+++ symlink.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+ln -s /Users/aaron/Documents/code/dotfiles/.aliases.local /Users/aaron
+ln -s /Users/aaron/Documents/code/dotfiles/.gitconfig.local /Users/aaron
+ln -s /Users/aaron/Documents/code/dotfiles/.laptop.local /Users/aaron
+ln -s /Users/aaron/Documents/code/dotfiles/.vimrc.bundles.local /Users/aaron
+ln -s /Users/aaron/Documents/code/dotfiles/.vimrc.local /Users/aaron
+ln -s /Users/aaron/Documents/code/dotfiles/.zshrc.local /Users/aaron
