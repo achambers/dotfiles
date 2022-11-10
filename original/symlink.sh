3b36a9686a3d13aab9eb91866e61aa5b7491b209
Aaron Chambers <aaron@Aarons-MacBook-Pro.local>
2022-11-10 22:50:17 +0000

    Delete old dotfiles

--- original/symlink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-ln -s /Users/aaron/Documents/code/dotfiles/.aliases.local /Users/aaron
-ln -s /Users/aaron/Documents/code/dotfiles/.gitconfig.local /Users/aaron
-ln -s /Users/aaron/Documents/code/dotfiles/.laptop.local /Users/aaron
-ln -s /Users/aaron/Documents/code/dotfiles/.vimrc.bundles.local /Users/aaron
-ln -s /Users/aaron/Documents/code/dotfiles/.vimrc.local /Users/aaron
-ln -s /Users/aaron/Documents/code/dotfiles/.zshrc.local /Users/aaron
